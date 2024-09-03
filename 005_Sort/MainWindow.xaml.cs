3483dd38961d6feb57e70b06fe7abcfb15273f98
BeeEye Dmu <bikang@konyang.ac.kr>
2024-09-03 11:55:06 +0900

    Add files via upload

--- 005_Sort/MainWindow.xaml.cs
+++ 005_Sort/MainWindow.xaml.cs
@@ -15,94 +15,93 @@ using System.Windows.Shapes;
 
 namespace _005_Sort
 {
-    /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// MainWindow.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    static int MAX = 1000000; // 백만
+    int[] a = new int[MAX];
+    int N = 0;      // 데이터 갯수
+    Random r = new Random();
+
+    public MainWindow()
     {
-        static int MAX = 1000000; // 백만
-        int[] a = new int[MAX];
-        int N = 0;      // 데이터 갯수
-
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void btnRandom_Click(object sender, RoutedEventArgs e)
-        {
-            N = int.Parse(txtData.Text);
-            Randomize();
-            PrintData();
-
-        }
-
-        private void Randomize()
-        {
-            Random r = new Random();
-            for (int i = 0; i < N; i++)
-                a[i] = r.Next(3 * N);            
-        }
-
-        private void PrintData()
-        {
-            for(int i=0; i<N; i++)
-            {
-                txtResult.Text += a[i] + " ";
-            }
-            txtResult.Text += "\n";
-        }
-
-        private void btnTime_Click(object sender, RoutedEventArgs e)
-        {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            BubbleSort();
-            watch.Stop();
-            long tickBubble = watch.ElapsedTicks;
-            long msBubble = watch.ElapsedMilliseconds;
-            txtBubble.Text = "버블 정렬 : " + tickBubble + " Ticks, "
-                + msBubble + " ms";
-
-            Randomize();
-            watch = System.Diagnostics.Stopwatch.StartNew();
-            QuickSort();
-            watch.Stop();
-            long tickQuick = watch.ElapsedTicks;
-            long msQuick = watch.ElapsedMilliseconds;
-            txtBubble.Text = "퀵 정렬 : " + tickQuick + " Ticks, "
-                + msQuick + " ms";
-
-            Randomize();
-            watch = System.Diagnostics.Stopwatch.StartNew();
-            MergeSort();
-            watch.Stop();
-            long tickMerge = watch.ElapsedTicks;
-            long msMerge = watch.ElapsedMilliseconds;
-            txtBubble.Text = "합병 정렬 : " + tickMerge + " Ticks, "
-                + msMerge + " ms";
-        }
-
-        private void MergeSort()
-        {
-            
-        }
-
-        private void QuickSort()
-        {
-            
-        }
-
-        private void BubbleSort()
-        {
-            for(int i=0; i<N; i++) 
-                for(int j=0; j<i; j++)
-                    if (a[i] < a[j])
-                    {
-                        int tmp = a[i];
-                        a[i] = a[j];
-                        a[j] = tmp;
-                    }
-            PrintData();
-        }
+      InitializeComponent();
     }
+
+    private void btnRandom_Click(object sender, RoutedEventArgs e)
+    {
+      txtResult.Text = "";
+      N = int.Parse(txtData.Text);
+      txtBubble.Text = "버블 정렬 : ";
+      txtQuick.Text = "퀵 정렬 : ";
+      txtMerge.Text = "합병 정렬 : ";
+      Randomize();
+      PrintData("랜덤 숫자");
+    }
+
+    private void Randomize()
+    {      
+      for (int i = 0; i < N; i++)
+        a[i] = r.Next(5 * N);   // 데이터 갯수의 5배까지의 랜덤 숫자 생성
+    }
+
+    private void PrintData(string s)
+    {
+      txtResult.Text += "\n" + s + "\n";
+      for (int i = 0; i < N; i++)
+      {
+        txtResult.Text += a[i] + " ";
+      }
+      txtResult.Text += "\n";
+    }
+
+    private void btnTime_Click(object sender, RoutedEventArgs e)
+    {
+      var watch = System.Diagnostics.Stopwatch.StartNew();
+      BubbleSort();      
+      watch.Stop();
+      PrintData("버블 정렬");
+      long tickBubble = watch.ElapsedTicks;
+      long msBubble = watch.ElapsedMilliseconds;
+      txtBubble.Text = "버블 정렬 : " + tickBubble + " Ticks, "
+          + msBubble + " ms";
+
+      Randomize();
+      watch = System.Diagnostics.Stopwatch.StartNew();
+      QuickSort(a, 0, N-1);      
+      watch.Stop();
+      PrintData("퀵 정렬");
+      long tickQuick = watch.ElapsedTicks;
+      long msQuick = watch.ElapsedMilliseconds;
+      txtQuick.Text = "퀵 정렬 : " + tickQuick + " Ticks, "
+          + msQuick + " ms";
+
+      Randomize();
+      watch = System.Diagnostics.Stopwatch.StartNew();
+      MergeSort(a, 0, N - 1);      
+      watch.Stop();
+      PrintData("합병 정렬");
+      long tickMerge = watch.ElapsedTicks;
+      long msMerge = watch.ElapsedMilliseconds;
+      txtMerge.Text = "합병 정렬 : " + tickMerge + " Ticks, "
+          + msMerge + " ms";
+    }
+
+    private void MergeSort(int[] a, int left, int right)
+    {
+
+    }
+
+    private void QuickSort(int[] a, int left, int right)
+    {
+  
+    }
+
+    private void BubbleSort()
+    {
+    
+    }
+  }
 }
